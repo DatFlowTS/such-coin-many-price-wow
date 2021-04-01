e3dd206a3a4dfbe2148f92e52bba1a42d3cc0db4
DatFlow <github@datflow.de>
2021-04-01 10:07:31 +0200

    .

--- src/listeners/Ready.ts
+++ src/listeners/Ready.ts
@@ -93,47 +93,67 @@ function recordingPresence (client: AkairoClient, passedTime: number) {
     })
 }
 
+function recordingReminder (client: AkairoClient) {
+    const checkString = fs.readFileSync(`${path}goTimestamp.json`, { encoding: 'utf-8' });
+
+    const checkArr = checkString.split(",");
+
+    const timestamp: number = parseInt(checkArr[0].replace(new RegExp('{"timestamp":'), ''));
+    const channelID: string = checkArr[1].replace(new RegExp('"channelID":"'), '').replace(new RegExp('"}'), '');
+
+    const channel: TextChannel = client.channels.cache.get(channelID) as TextChannel;
+    const now: number = Date.now();
+
+    var passedTime: number;
+
+    var reminder: string = 'null';
+
+    if (passedTime >= 6000000 && timestamp != 0) {
+        const data = {
+            "timestamp": 0,
+            "channelID": "0"
+        };
+        const dataString: string = JSON.stringify(data);
+	
+        fs.writeFile(`${botConfig.botDirectory}goTimestamp.json`, dataString, 'utf8', function (err) {
+            if(err) {
+                return console.log(err.stack)
+            }
+        })
+        passedTime = 0;
+        reminder = "**Die Aufnahme wurde automatisch gestoppt!**";
+    } else if (timestamp === 0) {
+        return;
+    } else {
+        passedTime = now - timestamp;
+        switch (timePassed(passedTime)) {
+            case 1:
+                reminder = `**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`;
+                break;
+            case 2:
+                reminder = `*es wird langsam eng...*\n**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`;
+                break;
+            case 3:
+                reminder = `*jetzt ist es lang genug!*\n**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`;
+                break;
+            case 4:
+                reminder = `*heute wieder überlänge?*\n**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (reminder !== 'null') {
+        return channel.send(reminder);
+    }
+    return;
+}
+
 function timePassed (passedTime: number): number {
     if (passedTime >= 4800000) return 4
     if (passedTime >= 3600000) return 3
     if (passedTime >= 2700000) return 2
     if (passedTime >= 600000) return 1
     return 0
-}
-
-function recordingReminder (client: AkairoClient) {
-    let checkString = fs.readFileSync(`${path}goTimestamp.json`, { encoding: 'utf-8' });
-
-    let checkArr = checkString.split(",");
-
-    var timestamp: number = parseInt(checkArr[0].replace(new RegExp('{"timestamp":'), ''));
-    var channelID: string = checkArr[1].replace(new RegExp('"channelID":"'), '').replace(new RegExp('"}'), '');
-
-    let channel: TextChannel = client.channels.cache.get(channelID) as TextChannel;
-    let now: number = Date.now();
-
-    var passedTime: number = timestamp >= now - 6000000 ? now - timestamp : 0;
-
-    var reminder: string = 'null';
-
-    switch (timePassed(passedTime)) {
-        case 1:
-            reminder = `**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`
-            break;
-        case 2:
-            reminder = `*es wird langsam eng...*\n**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`
-            break;
-        case 3:
-            reminder = `*jetzt ist es lang genug!*\n**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`
-            break;
-        case 4:
-            reminder = `*heute wieder überlänge?*\n**${moment.duration(passedTime).format('HH[h] mm[m] ss[s]')}**`
-            break;
-        default:
-            break;
-    }
-    if (reminder !== 'null') {
-        return channel.send(reminder);
-    }
-    return;
 }
